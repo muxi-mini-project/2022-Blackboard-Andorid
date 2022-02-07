a3c65f4ff40ded80833de092fd440466e563520f
flsdqm <592661681@qq.com>
2022-02-07 15:31:58 +0800

    刚写了登录页

---
+++ Blackboard/app/src/main/java/com/bignerdranch/android/blackboard/BoardActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.blackboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Blackboard/app/src/main/java/com/bignerdranch/android/blackboard/BoardFragment.java
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.blackboard;
+
+import androidx.fragment.app.Fragment;
+
+public class BoardFragment extends Fragment {
+}
