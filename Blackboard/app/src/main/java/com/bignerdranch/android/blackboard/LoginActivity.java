735f29ca636095660dc7c19d3054e62920a86e79
Nutcra <2428859434@qq.com>
2022-02-23 17:27:10 +0800

    from flsdqm

---
+++ Blackboard/app/src/main/java/com/bignerdranch/android/blackboard/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.bignerdranch.android.blackboard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private EditText mNumber;
+    private EditText mPassword;
+    private Retrofit mRetrofit;
+    private Login mLogin;
+    private String mToken;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mNumber = (EditText) findViewById(R.id.number);
+        mPassword = (EditText) findViewById(R.id.password);
+        mLoginButton = (Button) findViewById(R.id.login_button);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User user = new User(
+                        mNumber.getText().toString(),
+                        mPassword.getText().toString()
+                );
+
+                sendNetworkRequest(user);
+
+            }
+        });
+    }
+
+    private void sendNetworkRequest(User user) {
+        //创建OkHttp client
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);//此处有四个级别，body为显示所有
+
+        //判断是开发者模式，则调用OkHttp日志记录拦截器，方便debug
+        if(BuildConfig.DEBUG) {
+            okHttpClientBuilder.addInterceptor(logging);
+        }
+        //构建retrofit
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl("http://122.112.236.36:8080/api/v1\n" +
+                        "/login")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClientBuilder.build())
+                .build();
+
+        mLogin = mRetrofit.create(Login.class);
+        Call<User> call = mLogin.post(user);
+        //回调产生登录结果
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                mToken =response.body().getToken();
+                if (mToken!=null&&!mToken.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "登陆成功", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "登陆失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, "登陆失败", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
---
+++ Blackboard/app/src/main/java/com/bignerdranch/android/blackboard/MineFragment.java
@@ -0,0 +1,4 @@
+package com.bignerdranch.android.blackboard;
+
+public class MineFragment {
+}
