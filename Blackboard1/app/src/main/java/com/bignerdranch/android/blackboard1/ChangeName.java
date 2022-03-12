ef6433ad013ddbaa5b25acecffaff8fc7072bc20
flsdqm <592661681@qq.com>
2022-03-12 23:16:00 +0800

    重新传最新版

---
+++ Blackboard1/app/src/main/java/com/bignerdranch/android/blackboard1/ChangeName.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.blackboard1;
+
+public class ChangeName {
+    private String nickname;
+
+    public ChangeName(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
---
+++ Blackboard1/app/src/main/java/com/bignerdranch/android/blackboard1/Information.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.blackboard1;
+
+
+
+public class Information {
+        private String nickname;
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+
+}
