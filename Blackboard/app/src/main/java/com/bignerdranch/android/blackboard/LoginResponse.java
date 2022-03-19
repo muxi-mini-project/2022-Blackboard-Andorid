e7c05b14b83778d1e4be259eb10be16f4555f950
flsdqm <592661681@qq.com>
2022-03-19 22:33:36 +0800

    合并

---
+++ Blackboard/app/src/main/java/com/bignerdranch/android/blackboard/LoginResponse.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.blackboard;
+
+public class LoginResponse {
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    private Integer code;
+    private String message;
+    private String data;
+}
