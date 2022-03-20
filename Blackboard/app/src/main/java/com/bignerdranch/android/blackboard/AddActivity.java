f1b638bd9258c4450ffa3b21a6791c97d1c012e3
Nutcra <2428859434@qq.com>
2022-03-20 10:23:01 +0800

    after merge

---
+++ Blackboard/app/src/main/java/com/bignerdranch/android/blackboard/AddActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.blackboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
